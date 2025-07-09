fab06a2b94a95170052b02144846cc67bed5d74b
ergoproyect <ergoproyecto@gmail.com>
2025-07-08 19:12:07 -0500

    Delete app/__init__.py

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-# Configuraciones
-app.config['SECRET_KEY'] = 'tu-clave-secreta-aqui'
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-
-from app import routes
